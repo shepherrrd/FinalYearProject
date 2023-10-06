82c257fe336745b09343b9f6dd11f93a6baee017
shepherrrd <shefkid2@gmail.com>
2023-10-06 09:03:46 +0100

    Added Folder Structure

---
+++ FinalYearProject.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace FinalYearProject.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ FinalYearProject.Infrastructure/Data/Entities/NewsAggregatorUser.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+namespace FinalYearProject.Infrastructure.Data.Entities;
+
+public class NewsAggregatorUser : IdentityUser<long>
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? HandleName { get; set; }
+}
