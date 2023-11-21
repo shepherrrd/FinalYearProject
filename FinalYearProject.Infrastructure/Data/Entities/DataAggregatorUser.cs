0d3129150c7f27f847e5b0909b6a4d8b0f03a448
shepherrrd <shefkid2@gmail.com>
2023-11-21 21:05:23 +0100

    finished

--- FinalYearProject.Infrastructure/Data/Entities/DataAggregatorUser.cs
+++ FinalYearProject.Infrastructure/Data/Entities/DataAggregatorUser.cs
@@ -1,12 +1,14 @@
 ﻿
+using FinalYearProject.Infrastructure.Infrastructure.Utilities.Enums;
 using Microsoft.AspNetCore.Identity;
 
 namespace FinalYearProject.Infrastructure.Data.Entities;
 
-public class NewsAggregatorUser : IdentityUser<long>
+public class DataAggregatorUser : IdentityUser<long>
 {
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? MiddleName { get; set; }
-    public string? HandleName { get; set; }
+    public UserType UserType { get; set; }
+    public string? EmailAddress { get; set; }
 }
---
+++ FinalYearProject.Infrastructure/Infrastructure/Utilities/Enums/Enums.cs
@@ -0,0 +1,34 @@
+﻿
+using System.ComponentModel;
+using System.Reflection;
+
+namespace FinalYearProject.Infrastructure.Infrastructure.Utilities.Enums;
+
+public enum UserType
+{
+    [Description("Admin")]
+    Admin =1,
+    [Description("Hospital")]
+    Hospital,
+    [Description("Research Center")]
+    ResearchCenter
+}
+
+public static class Enums
+{
+    public static string GetDescription(this Enum GenericEnum)
+    {
+        Type genericEnumType = GenericEnum.GetType();
+        MemberInfo[] memberInfo = genericEnumType.GetMember(GenericEnum.ToString());
+        if ((memberInfo != null && memberInfo.Length > 0))
+        {
+            var _Attribs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+            if ((_Attribs != null && _Attribs.Length > 0))
+            {
+                return ((DescriptionAttribute)_Attribs.ElementAt(0)).Description;
+            }
+        }
+
+        return GenericEnum.ToString();
+    }
+}
